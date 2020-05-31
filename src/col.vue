bf7686a267520417b13fa9bd9533e7fdbaeeba53
liukuai <937613925@qq.com>
2020-05-31 17:59:40 +0800

    添加 row col

---
+++ src/col.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="col">
+        <slot></slot>
+    </div>
+</template>
+<scirpt>
+
+</scirpt>
+<style lang="scss" scoped>
+    .col{
+        height: 100px;
+        background: gray;
+        width: 50%;
+        border: 1px solid red;
+    }
+</style>
