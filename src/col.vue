16b23de9ad7f6606b7650e3d476c7a25e36319a8
liukuai <937613925@qq.com>
2020-06-06 22:09:09 +0800

    响应式基本完成

--- src/col.vue
+++ src/col.vue
@@ -57,7 +57,7 @@
 </script>
 <style lang="scss" scoped>
     .col{
-        height: 100px;
+        /*height: 100px;*/
         /*width: 50%;*/
 
         $class-prefix: col-;
@@ -73,7 +73,7 @@
                 margin-left: $n/24 * 100%;
             }
         }
-        @media (min-width: 577px) and (max-width: 768px) {
+        @media (min-width: 577px) {
             $class-prefix: col-ipad-;
             @for $n from 1 through 24{
                 &.#{$class-prefix}#{$n}{
@@ -88,7 +88,7 @@
                 }
             }
         }
-        @media (min-width: 769px) and (max-width: 992px) {
+        @media (min-width: 769px) {
             $class-prefix: col-narrow-pc-;
             @for $n from 1 through 24{
                 &.#{$class-prefix}#{$n}{
@@ -103,7 +103,7 @@
                 }
             }
         }
-        @media (min-width: 993px) and (max-width: 1200px) {
+        @media (min-width: 993px) {
             $class-prefix: col-pc-;
             @for $n from 1 through 24{
                 &.#{$class-prefix}#{$n}{
