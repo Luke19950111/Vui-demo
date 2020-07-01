d6cc26102da7c6f4de2283b23f78550e801825a2
liukuai <937613925@qq.com>
2020-07-01 21:28:28 +0800

    before收拢关闭方法：点按钮关闭时没有移除监听

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="popover" @click.stop="xxx">
-        <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+    <div class="popover" @click="onClick" ref="popover">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
         <span ref="triggerWrapper">
@@ -18,24 +18,56 @@
             }
         },
         methods: {
-            xxx() {
-                this.visible = !this.visible
-                    console.log(this.visible, '切换状态')
-                if(this.visible === true){
-                    this.$nextTick(() => {
-                        document.body.appendChild(this.$refs.contentWrapper)
-                        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-                        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-                        let eventHandler = ()=>{
-                            console.log('关闭')
-                            this.visible = false
-                            console.log('删除监听')
-                            document.removeEventListener('click', eventHandler)
-                        }
-                        document.addEventListener('click', eventHandler)
-                    })
+            positionContent() {
+                document.body.appendChild(this.$refs.contentWrapper)
+                let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+            },
+            listenToDocument() {
+                let eventHandler = (e)=>{
+                    console.log(e.target, 'ttt')
+                    console.log(this.$refs.popover.contains(e.target), 'abc')
+                    if (this.$refs.popover && (this.$refs.popover===e.target || this.$refs.popover.contains(e.target))) {
+                        return;
+                    }
+                    this.visible = false
+                    console.log('关闭2，移除监听')
+                    document.removeEventListener('click', eventHandler)
                 }
+                console.log('监听')
+                document.addEventListener('click', eventHandler)
+            },
+            onShow() {
+                setTimeout(() => {
+                    this.positionContent()
+                    this.listenToDocument()
+                }, 0)
+            },
+            onClick(event) {
+                if(this.$refs.triggerWrapper.contains(event.target)){
+                    this.visible = !this.visible
+                    if(this.visible === true){
+                        this.onShow()
+                    }else {
+                        console.log('关闭1')
+                    }
+                }
+
+                // this.visible = !this.visible
+                // if(this.visible === true){
+                //     this.$nextTick(() => {
+                //         document.body.appendChild(this.$refs.contentWrapper)
+                //         let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                //         this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                //         this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+                //         let eventHandler = ()=>{
+                //             this.visible = false
+                //             document.removeEventListener('click', eventHandler)
+                //         }
+                //         document.addEventListener('click', eventHandler)
+                //     })
+                // }
             }
         }
     }
