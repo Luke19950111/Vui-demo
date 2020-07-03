775d01e0838bb31783ba26a5b0a1f11a21c03ec8
liukuai <937613925@qq.com>
2020-07-03 22:23:36 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
         <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span ref="triggerWrapper" style="display: inline-block;">
             <slot></slot>
         </span>
     </div>
@@ -26,6 +26,9 @@
             },
             onClickDocument(e) {
                 if (this.$refs.popover && (this.$refs.popover===e.target || this.$refs.popover.contains(e.target))) {
+                    return;
+                }
+                if (this.$refs.contentWrapper && (this.$refs.contentWrapper===e.target || this.$refs.contentWrapper.contains(e.target))){
                     return;
                 }
                 this.close()
@@ -57,6 +60,8 @@
 </script>
 
 <style lang="scss" scoped>
+    $border-color: #333;
+    $border-radius: 4px;
     .popover{
         display: inline-block;
         vertical-align: top;
@@ -64,8 +69,32 @@
     }
     .content-wrapper{
         position: absolute;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
+        /*box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);*/
+        filter: drop-shadow(0 3px 3px rgba(0, 0, 0, 0.5));
+        background: white;
         transform: translateY(-100%);
+        margin-top: -10px;
+        padding: 0.5em 1em;
+        max-width: 20em;
+        word-break: break-all;
+        &::before, &::after{
+            content: '';
+            display: block;
+            border: 10px solid transparent;
+            width: 0;
+            height: 0;
+            position: absolute;
+            left: 10px;
+        }
+        &::before{
+            border-top-color: $border-color;
+            top: 100%;
+        }
+        &::after{
+            border-top-color: white;
+            top: calc(100% - 1px);
+        }
     }
 </style>
