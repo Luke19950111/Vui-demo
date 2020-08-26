3b4d9da73affbbda64a22fb747c2d592280e2f66
liukuai <liukuai0111@foxmail.com>
2020-08-26 14:36:53 +0800

    更新package.json

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 import Button from './src/button'
 import Icon from './src/icon'
 import ButtonGroup from './src/button-group'
+import Input from './src/input'
+import Row from './src/row'
+import Col from './src/col'
+import Layout from './src/layout'
+import Header from './src/header'
+import Content from './src/content'
+import Sider from './src/sider'
+import Footer from './src/footer'
+import Toast from './src/toast'
+import Plugin from './src/plugin'
+import Tabs from './src/tabs'
+import TabsHead from './src/tabs-head'
+import TabsItem from './src/tabs-item'
+import TabsBody from './src/tabs-body'
+import TabsPane from './src/tabs-pane'
+import Popover from './src/popover'
+import Collapse from './src/collapse'
+import CollapseItem from './src/collapse-item'
 
-export {Button, Icon, ButtonGroup}
+export {Button, Icon, ButtonGroup, Input, Row, Col, Layout, Header, Content, Sider, Footer, Toast, Plugin,
+Tabs, TabsHead, TabsItem, TabsBody, TabsBody, Popover, Collapse, CollapseItem}
